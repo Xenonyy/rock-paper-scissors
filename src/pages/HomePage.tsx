ec5ab206d26819ab2d1921a6926fe4112df08e5d
Xenonyy <gonda.armand@gmail.com>
2025-05-27 16:03:30 +0200

    feat: add rules modal

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import { Box } from '../components/common/Box';
 import { Container } from '../components/common/Container';
 import { Text } from '../components/common/Text';
 import { DefaultLayout } from '../components/layouts/DefaultLayout';
-import { RPSButton } from '../components/RPSButton/RPSButton';
+import clsx from 'clsx';
+import { RulesModal } from '../components/RulesModal/RulesModal';
+import { RPSButtonsContainer } from '../components/RPSButton/RPSButtonsContainer';
 
 export const HomePage = () => {
+  const [isModalActive, setIsModalActive] = useState(false);
+
+  const handleModalOpen = () => setIsModalActive(true);
+  const handleModalClose = () => setIsModalActive(false);
+
   const score = 12; // This should come from a state or props
+
   return (
     <DefaultLayout>
-      <Container className="[background-image:radial-gradient(circle_at_top,_#354063,_#2c3450,_#15183b)] p-8">
+      <Container
+        className={clsx('[background-image:radial-gradient(circle_at_top,_#354063,_#2c3450,_#15183b)] p-8', {
+          'opacity-20 pointer-events-none': isModalActive,
+        })}
+      >
         <Box className="uppercase font-semibold text-3xl w-[40%] border-solid border-3 border-gray-500 rounded-2xl flex-row justify-between px-6 py-4">
           <Box className="flex-col items-start">
             <Text text="Rock" />
@@ -20,15 +33,17 @@ export const HomePage = () => {
             <Text text={score} className="font-bold text-5xl" />
           </Box>
         </Box>
-        <Box className="flex gap-16 justify-center mt-14 ">
-          <RPSButton type="paper" onClick={() => console.log('Paper')} />
-          <RPSButton type="scissors" onClick={() => console.log('Scissors')} />
-        </Box>
-        <img src="bg-triangle.svg" alt="" aria-hidden className="relative z-0 h-48 w-48 -top-20 -mb-40" />
-        <Box className="">
-          <RPSButton type="rock" onClick={() => console.log('Rock')} />
+        <RPSButtonsContainer />
+        <Box className="justify-end w-full">
+          <button
+            className="cursor-pointer uppercase font-semibold tracking-widest border-solid border-3 hover:border-gray-300 transition-all duration-300 border-gray-500 rounded-2xl py-2 px-8"
+            onClick={handleModalOpen}
+          >
+            <Text text="Rules" />
+          </button>
         </Box>
       </Container>
+      <RulesModal src="image-rules.svg" active={isModalActive} close={handleModalClose} />
     </DefaultLayout>
   );
 };
