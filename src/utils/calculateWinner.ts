3bf067729fbb271aaed1b376887d55803c49cab4
Xenonyy <gonda.armand@gmail.com>
2025-05-27 11:10:37 +0200

    feat: add computer opponent decision making+determinig winner algorithm

---
+++ src/utils/calculateWinner.ts
@@ -0,0 +1,17 @@
+import type { RPSChoice } from '../types/RPSChoice';
+import type { GameState } from '../types/RPSState';
+
+export const calculateWinner = (player: RPSChoice, computer: RPSChoice): GameState => {
+  console.log(`Player chose: ${player}`);
+  console.log(`Computer chose: ${computer}`);
+  if (player === computer) return 'draw';
+
+  if (
+    (player === 'rock' && computer === 'scissors') ||
+    (player === 'paper' && computer === 'rock') ||
+    (player === 'scissors' && computer === 'paper')
+  ) {
+    return 'win';
+  }
+  return 'lose';
+};
---
+++ src/utils/getRandomComputerMove.ts
@@ -0,0 +1,9 @@
+import type { RPSChoice } from '../types/RPSChoice';
+
+export const getRandomComputerMove = (): RPSChoice => {
+  const choices: RPSChoice[] = ['rock', 'paper', 'scissors'];
+  const array = new Uint32Array(1);
+  crypto.getRandomValues(array);
+  console.log(choices[array[0] % 3]);
+  return choices[array[0] % 3];
+};
