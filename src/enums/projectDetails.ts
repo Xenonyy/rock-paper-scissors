8d913c3045aafb51ee26e663a88676972abbd30f
Xenonyy <gonda.armand@gmail.com>
2025-05-27 05:39:47 +0200

    add: project details

--- src/enums/projectDetails.ts
+++ src/enums/projectDetails.ts
@@ -1,6 +1,6 @@
 export const ProjectDetails = {
-  name: 'Project Name',
-  description: 'Project Description',
-  version: 'Project Version',
+  name: 'Rock Paper Scissors',
+  description: 'A rock paper scissors game built with React, TypeScript and Tailwind via Vite from Frontend Mentor.',
+  version: '0.0.1',
   year: '2025',
 };
