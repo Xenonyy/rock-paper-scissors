6cff2679ae91ecbf588efd3129631917482054cc
Xenonyy <gonda.armand@gmail.com>
2025-05-28 18:25:29 +0200

    improve WCAG compatibility

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,9 +2,9 @@ import { ProjectDetails } from '../../enums/projectDetails';
 
 export const Footer = () => {
   return (
-    <footer className="h-16 bg-violet-400 justify-around items-center flex flex-row">
-      <span className="text-white font-medium drop-shadow-md">&copy;{ProjectDetails.year} Gonda Armand</span>
-      <span className="text-white font-medium drop-shadow-md">{`Version: ${ProjectDetails.version}`}</span>
+    <footer className="h-16 bg-violet-400 text-black/80 justify-around items-center flex flex-row">
+      <span className=" font-medium drop-shadow-md">&copy;{ProjectDetails.year} Gonda Armand</span>
+      <span className=" font-medium drop-shadow-md">{`Version: ${ProjectDetails.version}`}</span>
     </footer>
   );
 };
