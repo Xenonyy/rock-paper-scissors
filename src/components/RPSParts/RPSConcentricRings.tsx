b4d261a49b0a4ab4a046fa31bdacf06dff26a1b0
Xenonyy <gonda.armand@gmail.com>
2025-05-29 06:32:33 +0200

    reduce concentric rings css delay to not make the animation feel slow

--- src/components/RPSParts/RPSConcentricRings.tsx
+++ src/components/RPSParts/RPSConcentricRings.tsx
@@ -5,7 +5,7 @@ export const Ring = ({ className }: { className: string }) => {
     <motion.div
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.5, delay: 0.2 }}
+      transition={{ duration: 0.5, delay: 0.1 }}
       className={`absolute rounded-full bg-white/5 z-0 ${className}`}
     />
   );
