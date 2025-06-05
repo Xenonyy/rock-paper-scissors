feecf67dfef8ba4a0dfae1bee557f1dbccd25b48
Xenonyy <gonda.armand@gmail.com>
2025-06-05 19:25:28 +0200

    bump version

--- src/enums/projectDetails.ts
+++ src/enums/projectDetails.ts
@@ -2,6 +2,6 @@ export const ProjectDetails = {
   name: 'Rock Paper Scissors',
   description:
     'A rock paper scissors game with a classic and an advanced mode built with React, TypeScript and Tailwind via Vite from Frontend Mentor.',
-  version: '1.2.0',
+  version: '1.3.0',
   year: '2025',
 };
